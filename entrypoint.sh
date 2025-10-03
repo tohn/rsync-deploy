08d2b07585c341f073d828ae7d29b275be5c4b5c
tohn <tohn@users.noreply.github.com>
2025-10-03 16:19:55 +0200

    set some variable defaults

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,12 +1,19 @@
 #!/bin/sh
+
 set -eu
 
+ARGS="${ARGS:--e -c -r --delete}"
 SSHPATH="$HOME/.ssh"
+SERVER_PORT="${SERVER_PORT:-22}"
+SSH_ARGS="${SSH_ARGS:-}"
+FOLDER="${FOLDER:-./}"
+SERVER_DEPLOY_STRING="$USERNAME@$SERVER_IP:$SERVER_DESTINATION"
+
 if [ ! -d "$SSHPATH" ]; then
   mkdir -p "$SSHPATH"
 fi
-echo "$DEPLOY_KEY" > "$SSHPATH/key"
+echo "$DEPLOY_KEY" >"$SSHPATH/key"
 chmod 600 "$SSHPATH/key"
-SERVER_DEPLOY_STRING="$USERNAME@$SERVER_IP:$SERVER_DESTINATION"
+
 # sync it up"
 sh -c "rsync $ARGS -e 'ssh -i $SSHPATH/key -o StrictHostKeyChecking=no -p $SERVER_PORT $SSH_ARGS' $GITHUB_WORKSPACE/$FOLDER $SERVER_DEPLOY_STRING"
